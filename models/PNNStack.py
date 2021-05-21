d102917390af346464e57133290cdeb32f58abc6
Zhang <6pz@pc0116960.ornl.gov>
2021-05-20 22:49:40 -0400

    add multi-task/multi-heads feature to PNN model

--- models/PNNStack.py
+++ models/PNNStack.py
@@ -6,7 +6,7 @@ from torch_geometric.nn import PNAConv, BatchNorm, global_mean_pool
 
 
 class PNNStack(torch.nn.Module):
-    def __init__(self, deg, input_dim, output_dim, hidden_dim, num_conv_layers):
+    def __init__(self, deg, input_dim, output_dim, num_nodes, hidden_dim, num_conv_layers, num_shared=1):
         super(PNNStack, self).__init__()
 
         aggregators = ["mean", "min", "max", "std"]
@@ -46,15 +46,35 @@ class PNNStack(torch.nn.Module):
             )
             self.convs.append(conv)
             self.batch_norms.append(BatchNorm(self.hidden_dim))
+        ############multiple heads/taks################
+        denselayers=[] #shared dense layers, before mutli-heads
+        for ishare in range(num_shared):
+            denselayers.append(Linear(self.hidden_dim, self.hidden_dim))
+            denselayers.append(ReLU())
+            self.shared=Sequential(*denselayers)
 
-        self.mlp = Sequential(
-            Linear(self.hidden_dim, 50),
-            ReLU(),
-            Linear(50, 25),
-            ReLU(),
-            Linear(25, output_dim),
-        )
+        # currently, only two types of outputs are considered, graph-level scalars and nodes-level vectors with num_nodes dimension, or mixed or the two
+        if output_dim < num_nodes: #all graph-level outputs
+            self.num_heads = output_dim
+            outputs_dims   = [1 for _ in range(self.num_heads)]
+        elif output_dim%num_nodes==0: #all node-level outputs
+            self.num_heads = output_dim//num_nodes
+            outputs_dims = [num_nodes for _ in range(self.num_heads)]
+        else: # mixed graph-level and node-level
+            self.num_heads = output_dim%num_nodes + output_dim//num_nodes
+            outputs_dims = [1 if ih<output_dim%num_nodes else num_nodes for ih in range(self.num_heads)]
 
+        self.num_heads = len(outputs_dims)  # number of heads/tasks
+        self.heads = ModuleList()
+        for ihead in range(self.num_heads):
+            mlp = Sequential(
+                Linear(self.hidden_dim, 50),
+                ReLU(),
+                Linear(50, 25),
+                ReLU(),
+                Linear(25, outputs_dims[ihead]),
+            )
+            self.heads.append(mlp)
     def forward(self, data):
         x, edge_index, batch = (
             data.x,
@@ -64,7 +84,12 @@ class PNNStack(torch.nn.Module):
         for conv, batch_norm in zip(self.convs, self.batch_norms):
             x = F.relu(batch_norm(conv(x=x, edge_index=edge_index)))
         x = global_mean_pool(x, batch)
-        return self.mlp(x)
+        ####
+        x = self.shared(x) # shared dense layers
+        outputs = []
+        for headloc in self.heads:
+            outputs.append(headloc(x))
+        return torch.cat(outputs, dim=1)
 
     def loss(self, pred, value):
         pred_shape = pred.shape
