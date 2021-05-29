1680b539b14a9c4fce9af47117c7834f1cbc4472
Massimiliano Lupo Pasini <lupopasinim@ornl.gov>
2021-05-29 01:12:25 -0400

    test fixed for multitaskin

--- test_trained_models.py
+++ test_trained_models.py
@@ -23,7 +23,7 @@ def best_models(
     models_dir="./reproducing_results/best_performing_models/",
     plot_results=False,
 ):
-    expected_error = [0.00008727, 0.09563177, 0.00056164]
+    expected_error = [0.0011474628477206312, 4.2980859073136316e-05, 0.09525399695377092]
     EPSILON = 1e-8
 
     available_models = sorted(os.listdir(models_dir))
