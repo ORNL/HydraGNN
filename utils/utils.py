97e0e3ccedbcc1ccf12338289c836f691e6d3516
Choi <choij@ornl.gov>
2021-07-28 20:08:30 -0400

    Fix for Summit and CADES

--- utils/utils.py
+++ utils/utils.py
@@ -18,14 +18,58 @@ from data_utils.dataset_descriptors import (
 )
 from utils.visualizer import Visualizer
 
+import re
+
+def parse_slurm_nodelist(nodelist):
+    """
+    Parse SLURM_NODELIST env string to get list of nodes.
+    Usage example:
+        parse_slurm_nodelist(os.environ["SLURM_NODELIST"])
+    Input examples:
+        "or-condo-g04"
+        "or-condo-g[05,07-08,13]"
+        "or-condo-g[05,07-08,13],or-condo-h[01,12]"
+    """
+    nlist = list()
+    for block, _ in re.findall(r'([\w-]+(\[[\d\-,]+\])*)', nodelist):
+        m = re.match(r'^(?P<prefix>[\w\-]+)\[(?P<group>.*)\]', block)
+        if m is None:
+            ## single node
+            nlist.append(block)
+        else:
+            ## multiple nodes
+            g = m.groups()
+            prefix = g[0]
+            for sub in g[1].split(','):
+                if '-' in sub:
+                    start, end = re.match(r'(\d+)-(\d+)', sub).groups()
+                    fmt = '%%0%dd'%(len(start))
+                    for i in range(int(start), int(end)+1):
+                        node = prefix+fmt%i
+                        nlist.append(node)
+                else:
+                    node = prefix+sub
+                    nlist.append(node)
+
+    return nlist
+
 
 def get_comm_size_and_rank():
-    world_size = 1
+    world_size = None
     world_rank = 0
-    try:
-        world_size = os.environ["OMPI_COMM_WORLD_SIZE"]
-        world_rank = os.environ["OMPI_COMM_WORLD_RANK"]
-    except KeyError:
+
+    if os.getenv('OMPI_COMM_WORLD_SIZE') and os.getenv('OMPI_COMM_WORLD_RANK'):
+        ## Summit
+        world_size = int(os.environ["OMPI_COMM_WORLD_SIZE"])
+        world_rank = int(os.environ["OMPI_COMM_WORLD_RANK"])
+    elif os.getenv('SLURM_NPROCS') and os.getenv('SLURM_PROCID'):
+        ## CADES
+        world_size = int(os.environ["SLURM_NPROCS"])
+        world_rank = int(os.environ["SLURM_PROCID"])
+
+    ## Fall back to default
+    if world_size is None:
+        world_size = 1
         print("DDP has to be initialized within a job - Running in sequential mode")
 
     return int(world_size), int(world_rank)
@@ -40,26 +84,29 @@ def setup_ddp():
     distributed_data_parallelism = False
     world_size, world_rank = get_comm_size_and_rank()
 
-    ## source: https://www.olcf.ornl.gov/wp-content/uploads/2019/12/Scaling-DL-on-Summit.pdf
-    ## The following is Summit specific
-    import subprocess
-    get_master = "echo $(cat {} | sort | uniq | grep -v batch | grep -v login | head -1 )".format(os.environ['LSB_DJOB_HOSTFILE'])
-    master_addr = str(subprocess.check_output(get_master, shell=True))[2:-3]
-    master_port = "23456"
+    ## Default setting
+    master_addr = "127.0.0.1"
+    master_port = "8889"
+
+    if os.getenv('LSB_DJOB_HOSTFILE') is not None:
+        ## source: https://www.olcf.ornl.gov/wp-content/uploads/2019/12/Scaling-DL-on-Summit.pdf
+        ## The following is Summit specific
+        import subprocess
+        get_master = "echo $(cat {} | sort | uniq | grep -v batch | grep -v login | head -1 )".format(os.environ['LSB_DJOB_HOSTFILE'])
+        master_addr = str(subprocess.check_output(get_master, shell=True))[2:-3]
+    elif os.getenv('SLURM_NODELIST') is not None:
+        master_addr = parse_slurm_nodelist(os.environ["SLURM_NODELIST"])[0]
 
     try:
-        world_size = os.environ["OMPI_COMM_WORLD_SIZE"]
-        world_rank = os.environ["OMPI_COMM_WORLD_RANK"]
         os.environ["MASTER_ADDR"] = master_addr
         os.environ["MASTER_PORT"] = master_port
-        os.environ["WORLD_SIZE"] = world_size
-        os.environ["RANK"] = world_rank
+        os.environ["WORLD_SIZE"] = str(world_size)
+        os.environ["RANK"] = str(world_rank)
         if not dist.is_initialized():
             dist.init_process_group(
                 backend=backend, rank=int(world_rank), world_size=int(world_size)
             )
         distributed_data_parallelism = True
-
     except KeyError:
         print("DDP has to be initialized within a job - Running in sequential mode")
 
