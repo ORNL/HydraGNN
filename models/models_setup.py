3e6c506985c7acb66e879c64be2468ce9f8c6b9d
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2021-10-12 15:29:30 -0400

    model_setup fixed

--- models/models_setup.py
+++ models/models_setup.py
@@ -8,7 +8,7 @@ from .PNAStack import PNAStack
 from .GATStack import GATStack
 from .MFCStack import MFCStack
 
-from utils.distributed_environment import get_comm_size_and_rank
+from utils.distributed import get_comm_size_and_rank
 from utils.print_utils import print_distributed
 
 
