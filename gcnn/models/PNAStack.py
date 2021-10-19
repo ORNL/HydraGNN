20add7626c326bcc28a0ba7538734e7da2ddd6e4
Sam Reeve <6740307+streeve@users.noreply.github.com>
2021-10-19 10:29:04 -0400

    Add BSD-3 license

--- gcnn/models/PNAStack.py
+++ gcnn/models/PNAStack.py
@@ -1,3 +1,14 @@
+##############################################################################
+# Copyright (c) 2021, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
+
 import torch
 import torch.nn.functional as F
 from torch.nn import ModuleList
--- gcnn/postprocess/postprocess.py
+++ gcnn/postprocess/postprocess.py
@@ -1,3 +1,15 @@
+##############################################################################
+# Copyright (c) 2021, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
+
+
 def output_denormalize(y_minmax, true_values, predicted_values):
     # Fixme, should be improved later
     for ihead in range(len(y_minmax)):
