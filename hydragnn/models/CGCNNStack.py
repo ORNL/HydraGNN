de9a3af31ca6bd7a98bc234518ee2cd8fb154b8c
Pei Zhang <zhangp1@ornl.gov>
2021-10-18 23:22:04 -0400

    placeholder for conv node feature in CGCNN update printing; relax error for CGCNN temporally

--- hydragnn/models/CGCNNStack.py
+++ hydragnn/models/CGCNNStack.py
@@ -38,15 +38,40 @@ class CGCNNStack(Base):
         # We use input dimension (first argument of constructor) also as hidden dimension (second argument of constructor)
         super().__init__(input_dim, input_dim, dropout, num_conv_layers)
 
+        self.__conv_node_features__()
         super()._multihead(
-            output_dim,
-            num_nodes,
-            output_type,
-            config_heads,
-            ilossweights_hyperp,
-            loss_weights,
-            ilossweights_nll,
+            num_nodes, ilossweights_hyperp, loss_weights, ilossweights_nll
         )
+
+    def __conv_node_features__(self):
+        # convolutional layers for node level predictions
+        # two ways to implement node features from here:
+        # 1. one graph for all node features
+        # 2. one graph for one node features (currently implemented)
+        self.convs_node_hidden = ModuleList()
+        self.batch_norms_node_hidden = ModuleList()
+        self.convs_node_output = ModuleList()
+        self.batch_norms_node_output = ModuleList()
+
+        node_feature_ind = [
+            i for i, head_type in enumerate(self.head_type) if head_type == "node"
+        ]
+        if len(node_feature_ind) == 0:
+            return
+
+        self.num_conv_layers_node = self.config_heads["node"]["num_headlayers"]
+        self.hidden_dim_node = self.config_heads["node"]["dim_headlayers"]
+
+        print(
+            "Warning: conv for node features decoder part not ready yet! Switch to shared mlp for prediction"
+        )
+        self.config_heads["node"]["type"] = "mlp"
+        self.config_heads["node"]["share_mlp"] = True
+
+        # fixme: CGConv layer alone will present the same out dimension with the input, instead of having different "in_channels" and "out_channels" as in the other conv layers;
+        # so to predict output node features with different dimensions from the input node feature's, CGConv can be
+        # combined with, e.g.,mlp
+        return
 
     def get_conv(self, input_dim, _):
         return CGConv(
