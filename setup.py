eb6b33e0dbd0f915afce2ac0ad6057392d1b1fe4
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2021-11-16 12:15:16 -0500

    setup file added (#25)
    * setup file added
    
    Co-authored-by: Sam Reeve <6740307+streeve@users.noreply.github.com>
    
    * Add requirements to setup
    
    * fixup requirements (unused packages)
    
    * Ignore installation files
    
    Co-authored-by: Sam Reeve <6740307+streeve@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+# Note: setup() has access to cmd arguments of the setup.py script via sys.argv
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+install_requires = [
+    "pickle5",
+    "matplotlib",
+    "tqdm",
+    "tensorboard",
+    "torch>=1.8",
+    "torch-geometric>=1.7.2",
+    "torch-scatter",
+    "torch-sparse",
+]
+test_requires = ["black", "pytest", "pytest-mpi"]
+
+setup(
+    name="HydraGNN",
+    version="1.0",
+    package_dir={"hydragnn": "hydragnn"},
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={"test": test_requires},
+    description="Distributed PyTorch implementation of multi-headed graph convolutional neural networks",
+    license="BSD-3",
+    long_description=read("README.md"),
+    url="https://github.com/ORNL/HydraGNN",
+    author="Massimiliano Lupo Pasini, Samuel Temple Reeve, Pei Zhang, Jong Youl Choi",
+    author_email="lupopasinim@ornl.gov",
+)
