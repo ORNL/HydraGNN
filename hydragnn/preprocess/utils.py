4d21a69b459a223d094deccf367378a3f81d8c1f
Pei Zhang <zhangp1@ornl.gov>
2021-11-19 11:14:32 -0500

    move check_if_graph_size_constant to preprocess.utils in rebase

---
+++ hydragnn/preprocess/utils.py
@@ -0,0 +1,22 @@
+##############################################################################
+# Copyright (c) 2021, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
+
+
+def check_if_graph_size_constant(train_loader, val_loader, test_loader):
+    graph_size_variable = False
+    nodes_num_list = []
+    for loader in [train_loader, val_loader, test_loader]:
+        for data in loader:
+            nodes_num_list.extend(data.num_nodes_list.tolist())
+            if len(list(set(nodes_num_list))) > 1:
+                graph_size_variable = True
+                return graph_size_variable
+    return graph_size_variable
