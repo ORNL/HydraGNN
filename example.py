38f39294980d0adce145c736531b6405318124d9
Acer, Seher <acers@ornl.gov>
2021-11-22 15:51:49 -0500

    Partial implementation of PI3NN

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
 import hydragnn
 
-hydragnn.run_training("./examples/configuration.json")
+#hydragnn.run_training("./examples/configuration.json")
+hydragnn.run_uncertainty("logs/PNAStack-r-7-mnnn-5-ncl-6-hd-5-ne-200-lr-0.001-bs-64-data-FePt_32atoms-node_ft-0-task_weights-1.0-/config.json")
+
+
