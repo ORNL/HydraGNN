d56dce656577a3d709d67d706603d0e4b59d84b7
Acer, Seher <acers@ornl.gov>
2021-11-22 15:51:49 -0500

    Partial implementation of PI3NN

--- hydragnn/__init__.py
+++ hydragnn/__init__.py
@@ -1,3 +1,4 @@
 from . import preprocess, models, train, postprocess, utils
 from .run_training import run_training
 from .run_prediction import run_prediction
+from .run_uncertainty import run_uncertainty
