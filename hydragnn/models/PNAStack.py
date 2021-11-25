7b0e2dfc31f484fc040eacc8e29521711490eebb
Massimiliano Lupo Pasini <7ml@ornl.gov>
2021-11-25 09:32:51 -0500

    edge_dim is an integer

--- hydragnn/models/PNAStack.py
+++ hydragnn/models/PNAStack.py
@@ -26,7 +26,7 @@ class PNAStack(Base):
         num_nodes: int,
         hidden_dim: int,
         config_heads: {},
-        edge_dim: list = None,
+        edge_dim: int = None,
         dropout: float = 0.25,
         num_conv_layers: int = 16,
         ilossweights_hyperp: int = 1,  # if =1, considering weighted losses for different tasks and treat the weights as hyper parameters
