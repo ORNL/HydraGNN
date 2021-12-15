d460287295da56b793e0fb56a6fcc0eebdff096e
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2021-12-15 20:00:51 +0000

    mapping of degree tensor to GPU

--- hydragnn/utils/model.py
+++ hydragnn/utils/model.py
@@ -16,7 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torch_geometric.data import Data
 from torch_geometric.utils import degree
 
-from hydragnn.utils.distributed import get_comm_size_and_rank, is_model_distributed
+from hydragnn.utils.distributed import get_comm_size_and_rank, get_device, is_model_distributed
 from collections import OrderedDict
 
 
@@ -63,7 +63,7 @@ def load_existing_model(model, model_name, path="./logs/"):
 
 
 def calculate_PNA_degree(dataset: [Data], max_neighbours):
-    deg = torch.zeros(max_neighbours + 1, dtype=torch.long)
+    deg = torch.zeros(max_neighbours + 1, dtype=torch.long).to(get_device())
     for data in dataset:
         d = degree(data.edge_index[1], num_nodes=data.num_nodes, dtype=torch.long)
         deg += torch.bincount(d, minlength=deg.numel())
