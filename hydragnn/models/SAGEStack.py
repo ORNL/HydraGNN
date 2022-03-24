45339b6ac10d1f375b086d4a2d42a328de3026c1
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2022-03-24 13:04:48 -0400

    SAGEStack added (#109)
    * SAGEStack added
    
    * Update SAGEStack and test accuracy
    
    * Simplify sage inputs
    
    Co-authored-by: Massimiliano Lupo Pasini <7ml@ornl.gov>
    Co-authored-by: Sam Reeve <6740307+streeve@users.noreply.github.com>

---
+++ hydragnn/models/SAGEStack.py
@@ -0,0 +1,32 @@
+##############################################################################
+# Copyright (c) 2021, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.nn import ModuleList
+from torch_geometric.nn import SAGEConv, BatchNorm
+
+from .Base import Base
+
+
+class SAGEStack(Base):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_conv(self, input_dim, output_dim):
+        return SAGEConv(
+            in_channels=input_dim,
+            out_channels=output_dim,
+        )
+
+    def __str__(self):
+        return "SAGEStack"
