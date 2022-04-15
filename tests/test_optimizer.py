bd4485bff27dbc1da07b2f4129fef6cb9dc9c7e2
Massimiliano Lupo Pasini <7ml@ornl.gov>
2022-04-15 13:45:35 -0400

    requirements-optional.txt and test_optimizer.py added

---
+++ tests/test_optimizer.py
@@ -0,0 +1,120 @@
+##############################################################################
+# Copyright (c) 2021, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
+
+import os, json
+import pytest
+
+import shutil
+
+import hydragnn, tests
+from hydragnn.utils.distributed import get_device_name
+
+
+# Loss function unit test called by pytest wrappers.
+# Note the intent of this test is to make sure all interfaces work - it does not assert anything
+def unittest_optimizers(optimizer_type, ci_input, overwrite_data=False):
+    world_size, rank = hydragnn.utils.get_comm_size_and_rank()
+
+    os.environ["SERIALIZED_DATA_PATH"] = os.getcwd()
+
+    # Read in config settings and override model type.
+    config_file = os.path.join(os.getcwd(), "tests/inputs", ci_input)
+    with open(config_file, "r") as f:
+        config = json.load(f)
+
+    # use pkl files if exist by default
+    for dataset_name in config["Dataset"]["path"].keys():
+        if dataset_name == "total":
+            pkl_file = (
+                os.environ["SERIALIZED_DATA_PATH"]
+                + "/serialized_dataset/"
+                + config["Dataset"]["name"]
+                + ".pkl"
+            )
+        else:
+            pkl_file = (
+                os.environ["SERIALIZED_DATA_PATH"]
+                + "/serialized_dataset/"
+                + config["Dataset"]["name"]
+                + "_"
+                + dataset_name
+                + ".pkl"
+            )
+        if os.path.exists(pkl_file):
+            config["Dataset"]["path"][dataset_name] = pkl_file
+
+    if rank == 0:
+        num_samples_tot = 500
+        # check if serialized pickle files or folders for raw files provided
+        pkl_input = False
+        if list(config["Dataset"]["path"].values())[0].endswith(".pkl"):
+            pkl_input = True
+        # only generate new datasets, if not pkl
+        if not pkl_input:
+            for dataset_name, data_path in config["Dataset"]["path"].items():
+                if overwrite_data:
+                    shutil.rmtree(data_path)
+                if not os.path.exists(data_path):
+                    os.makedirs(data_path)
+                if dataset_name == "total":
+                    num_samples = num_samples_tot
+                elif dataset_name == "train":
+                    num_samples = int(
+                        num_samples_tot
+                        * config["NeuralNetwork"]["Training"]["perc_train"]
+                    )
+                elif dataset_name == "test":
+                    num_samples = int(
+                        num_samples_tot
+                        * (1 - config["NeuralNetwork"]["Training"]["perc_train"])
+                        * 0.5
+                    )
+                elif dataset_name == "validate":
+                    num_samples = int(
+                        num_samples_tot
+                        * (1 - config["NeuralNetwork"]["Training"]["perc_train"])
+                        * 0.5
+                    )
+                if not os.listdir(data_path):
+                    tests.deterministic_graph_data(
+                        data_path, number_configurations=num_samples
+                    )
+
+    config["NeuralNetwork"]["Training"]["optimizer"] = optimizer_type
+    config["NeuralNetwork"]["Training"]["num_epoch"] = 2
+
+    # Make sure training works with each optimizer.
+    if config["NeuralNetwork"]["Training"]["optimizer"] == "LAMB":
+
+        deepspeed_available = True
+        try:
+            import deepspeed
+        except ImportError:
+            deepspeed_available = False
+
+        try:
+            hydragnn.run_training(config)
+        except:
+            if not (deepspeed_available or "cpu" != get_device_name()):
+                return True
+            else:
+                return False
+    else:
+        hydragnn.run_training(config)
+
+
+# Test all supported loss function types. Separate input file because only 2 steps are run.
+@pytest.mark.parametrize(
+    "optimizer_type",
+    ["SGD", "Adam", "Adadelta", "Adagrad", "Adamax", "AdamW", "RMSprop", "LAMB"],
+)
+def pytest_optimizers(optimizer_type, ci_input="ci.json", overwrite_data=False):
+    unittest_optimizers(optimizer_type, ci_input, overwrite_data)
