adf6582589c463d05d4147a191b1cd93c00d5c7a
Choi <choij@ornl.gov>
2022-04-18 23:27:43 -0400

    wip: download

--- examples/ogb/train_gap.py
+++ examples/ogb/train_gap.py
@@ -7,8 +7,6 @@ import sys
 from tqdm import tqdm
 import mpi4py
 
-mpi4py.rc.initialize = False
-mpi4py.rc.finalize = False
 from mpi4py import MPI
 from itertools import chain
 import argparse
@@ -25,6 +23,8 @@ import torch
 
 import warnings
 
+from torch_geometric.data import download_url, extract_tar
+
 warnings.filterwarnings("error")
 
 
@@ -35,205 +35,139 @@ class AdioGGO:
         self.rank = comm.Get_rank()
         self.size = comm.Get_size()
 
-        self.dataset = list()
+        self.dataset = dict()
         self.adios = ad2.ADIOS()
         self.io = self.adios.DeclareIO(self.filename)
 
-    def add(self, data: torch_geometric.data.Data):
+    def add(self, label, data: torch_geometric.data.Data):
+        if label not in self.dataset:
+            self.dataset[label] = list()
         if isinstance(data, list):
-            for x in data:
-                self.dataset.append(x)
+            self.dataset[label].extend(data)
         elif isinstance(data, torch_geometric.data.Data):
-            self.dataset.append(data)
+            self.dataset[label].append(data)
         else:
             raise Exception("Unsuppored data type yet.")
 
     def save(self):
         t0 = time.time()
         info("Adios saving:", self.filename)
-        ns = self.comm.allgather(len(self.dataset))
-        ns_offset = sum(ns[: self.rank])
-
-        self.io.DefineAttribute("ndata", np.array(sum(ns)))
-        if len(self.dataset) > 0:
-            data = self.dataset[0]
-            self.io.DefineAttribute("keys", data.keys)
-            keys = sorted(data.keys)
-
         self.writer = self.io.Open(self.filename, ad2.Mode.Write, self.comm)
-        for k in keys:
-            arr_list = [data[k].cpu().numpy() for data in self.dataset]
-            m0 = np.min([x.shape for x in arr_list], axis=0)
-            m1 = np.max([x.shape for x in arr_list], axis=0)
-            wh = np.where(m0 != m1)[0]
-            assert len(wh) < 2
-            vdim = wh[0] if len(wh) == 1 else 1
-            val = np.concatenate(arr_list, axis=vdim)
-            assert val.data.contiguous
-            shape_list = self.comm.allgather(list(val.shape))
-            offset = [0,] * len(val.shape)
-            for i in range(rank):
-                offset[vdim] += shape_list[i][vdim]
-            global_shape = shape_list[0]
-            for i in range(1, self.size):
-                global_shape[vdim] += shape_list[i][vdim]
-            # info ("k,val shape", k, global_shape, offset, val.shape)
-            var = self.io.DefineVariable(
-                k, val, global_shape, offset, val.shape, ad2.ConstantDims
-            )
-            self.writer.Put(var, val, ad2.Mode.Sync)
+        for label in self.dataset:
+            if len(self.dataset[label]) < 1:
+                continue
+            print("label", label)
+            ns = self.comm.allgather(len(self.dataset[label]))
+            ns_offset = sum(ns[: self.rank])
 
-            self.io.DefineAttribute("%s/variable_dim" % k, np.array(vdim))
+            self.io.DefineAttribute("%s/ndata" % label, np.array(sum(ns)))
+            if len(self.dataset[label]) > 0:
+                data = self.dataset[label][0]
+                self.io.DefineAttribute("%s/keys" % label, data.keys)
+                keys = sorted(data.keys)
 
-            vcount = np.array([x.shape[vdim] for x in arr_list])
-            assert len(vcount) == len(self.dataset)
-
-            offset_arr = np.zeros_like(vcount)
-            offset_arr[1:] = np.cumsum(vcount)[:-1]
-            offset_arr += offset[vdim]
-
-            var = self.io.DefineVariable(
-                "%s/variable_count" % k,
-                vcount,
-                [sum(ns),],
-                [ns_offset,],
-                [len(vcount),],
-                ad2.ConstantDims,
-            )
-            self.writer.Put(var, vcount, ad2.Mode.Sync)
-
-            var = self.io.DefineVariable(
-                "%s/variable_offset" % k,
-                offset_arr,
-                [sum(ns),],
-                [ns_offset,],
-                [len(vcount),],
-                ad2.ConstantDims,
-            )
-            self.writer.Put(var, offset_arr, ad2.Mode.Sync)
-
-        self.writer.Close()
-        t1 = time.time()
-        info("Adios saving time (sec): ", (t1 - t0))
-
-    def load(self):
-        t0 = time.time()
-        info("Adios reading:", self.filename)
-        with ad2.open(self.filename, "r", MPI.COMM_SELF) as f:
-            vars = f.available_variables()
-            keys = f.read_attribute_string("keys")
-            ndata = f.read_attribute("ndata").item()
-
-            variable_count = dict()
-            variable_offset = dict()
-            variable_dim = dict()
-            data = dict()
             for k in keys:
-                variable_count[k] = f.read("%s/variable_count" % k)
-                variable_offset[k] = f.read("%s/variable_offset" % k)
-                variable_dim[k] = f.read_attribute("%s/variable_dim" % k).item()
-                ## load full data first
-                data[k] = f.read(k)
-            t2 = time.time()
-            info("Adios reading time (sec): ", (t2 - t0))
-
-            for i in iterate_tqdm(range(ndata), verbosity_level=2):
-                data_object = torch_geometric.data.Data()
-                for k in keys:
-                    shape = f.available_variables()[k]["Shape"]
-                    ishape = [int(x.strip(","), 16) for x in shape.strip().split()]
-                    start = [0,] * len(ishape)
-                    count = ishape
-                    vdim = variable_dim[k]
-                    start[vdim] = variable_offset[k][i]
-                    count[vdim] = variable_count[k][i]
-                    slice_list = list()
-                    for n0, n1 in zip(start, count):
-                        slice_list.append(slice(n0, n0 + n1))
-                    val = data[k][tuple(slice_list)]
-
-                    _ = torch.tensor(val)
-                    exec("data_object.%s = _" % (k))
-                self.dataset.append(data_object)
-
-        t1 = time.time()
-        info("Data loading time (sec): ", (t1 - t0))
-        return self.dataset
-
-    def save_v1(self):
-        t0 = time.time()
-        info("Adios saving:", self.filename)
-        self.writer = self.io.Open(self.filename, ad2.Mode.Write, self.comm)
-        ns = self.comm.allgather(len(self.dataset))
-        ns.insert(0, 0)
-        offset = sum(ns[: rank + 1])
-
-        self.io.DefineAttribute("ndata", np.array(sum(ns)))
-        if len(self.dataset) > 0:
-            self.io.DefineAttribute("keys", self.dataset[0].keys)
-
-        for i, data in enumerate(self.dataset):
-            varinfo_list = list()
-            for k in data.keys:
-                val = data[k].cpu().numpy()
+                arr_list = [data[k].cpu().numpy() for data in self.dataset[label]]
+                m0 = np.min([x.shape for x in arr_list], axis=0)
+                m1 = np.max([x.shape for x in arr_list], axis=0)
+                wh = np.where(m0 != m1)[0]
+                assert len(wh) < 2
+                vdim = wh[0] if len(wh) == 1 else 1
+                val = np.concatenate(arr_list, axis=vdim)
                 assert val.data.contiguous
-                varinfo_list.append((k, val))
-
-            gname = "data_%d" % (i + offset)
-            for vname, val in varinfo_list:
+                shape_list = self.comm.allgather(list(val.shape))
+                offset = [0,] * len(val.shape)
+                for i in range(rank):
+                    offset[vdim] += shape_list[i][vdim]
+                global_shape = shape_list[0]
+                for i in range(1, self.size):
+                    global_shape[vdim] += shape_list[i][vdim]
+                # info ("k,val shape", k, global_shape, offset, val.shape)
                 var = self.io.DefineVariable(
-                    "%s/%s" % (gname, vname), val, [], [], val.shape, ad2.ConstantDims
+                    "%s/%s" % (label, k),
+                    val,
+                    global_shape,
+                    offset,
+                    val.shape,
+                    ad2.ConstantDims,
                 )
                 self.writer.Put(var, val, ad2.Mode.Sync)
 
+                self.io.DefineAttribute(
+                    "%s/%s/variable_dim" % (label, k), np.array(vdim)
+                )
+
+                vcount = np.array([x.shape[vdim] for x in arr_list])
+                assert len(vcount) == len(self.dataset[label])
+
+                offset_arr = np.zeros_like(vcount)
+                offset_arr[1:] = np.cumsum(vcount)[:-1]
+                offset_arr += offset[vdim]
+
+                var = self.io.DefineVariable(
+                    "%s/%s/variable_count" % (label, k),
+                    vcount,
+                    [sum(ns),],
+                    [ns_offset,],
+                    [len(vcount),],
+                    ad2.ConstantDims,
+                )
+                self.writer.Put(var, vcount, ad2.Mode.Sync)
+
+                var = self.io.DefineVariable(
+                    "%s/%s/variable_offset" % (label, k),
+                    offset_arr,
+                    [sum(ns),],
+                    [ns_offset,],
+                    [len(vcount),],
+                    ad2.ConstantDims,
+                )
+                self.writer.Put(var, offset_arr, ad2.Mode.Sync)
+
         self.writer.Close()
         t1 = time.time()
         info("Adios saving time (sec): ", (t1 - t0))
 
-    def load_v1(self):
-        t0 = time.time()
-        info("Adios reading:", self.filename)
-        with ad2.open(self.filename, "r", MPI.COMM_SELF) as f:
-            keys = f.read_attribute_string("keys")
-            ndata = f.read_attribute("ndata").item()
-            vars = f.available_variables()
-            for i in range(ndata):
-                data_object = torch_geometric.data.Data()
-                for k in keys:
-                    _ = torch.tensor(f.read("data_%d/%s" % (i, k)))
-                    exec("data_object.%s = _" % (k))
-                self.dataset.append(data_object)
-
-        t1 = time.time()
-        info("Adios reading time (sec): ", (t1 - t0))
-        return self.dataset
-
 
 class OGBDataset(torch.utils.data.Dataset):
-    def __init__(self, filename):
+    def __init__(self, filename, label):
+        self.url = 'https://www.dropbox.com/s/7qe3zppbicw9vxj/ogb_gap.bp.tar.gz?dl=0'
         t0 = time.time()
         self.filename = filename
+        self.label = label
         self.data_object = dict()
         info("Adios reading:", self.filename)
+        if not os.path.exists(filename):
+            self.raw_dir = os.path.dirname(self.filename)
+            self.download()
+        MPI.COMM_WORLD.Barrier()
         with ad2.open(self.filename, "r", MPI.COMM_SELF) as f:
             self.vars = f.available_variables()
-            self.keys = f.read_attribute_string("keys")
-            self.ndata = f.read_attribute("ndata").item()
+            self.keys = f.read_attribute_string("%s/keys" % label)
+            self.ndata = f.read_attribute("%s/ndata" % label).item()
 
             self.variable_count = dict()
             self.variable_offset = dict()
             self.variable_dim = dict()
             self.data = dict()
             for k in self.keys:
-                self.variable_count[k] = f.read("%s/variable_count" % k)
-                self.variable_offset[k] = f.read("%s/variable_offset" % k)
-                self.variable_dim[k] = f.read_attribute("%s/variable_dim" % k).item()
+                self.variable_count[k] = f.read("%s/%s/variable_count" % (label, k))
+                self.variable_offset[k] = f.read("%s/%s/variable_offset" % (label, k))
+                self.variable_dim[k] = f.read_attribute(
+                    "%s/%s/variable_dim" % (label, k)
+                ).item()
                 ## load full data first
-                self.data[k] = f.read(k)
+                self.data[k] = f.read("%s/%s" % (label, k))
             t2 = time.time()
             info("Adios reading time (sec): ", (t2 - t0))
         t1 = time.time()
         info("Data loading time (sec): ", (t1 - t0))
+
+    def download(self):
+        path = download_url(self.url, './')
+        print ("path", path)
+        extract_tar(path, './')
+        # os.unlink(path)
 
     def __len__(self):
         return self.ndata
@@ -244,7 +178,7 @@ class OGBDataset(torch.utils.data.Dataset):
         else:
             data_object = torch_geometric.data.Data()
             for k in self.keys:
-                shape = self.vars[k]["Shape"]
+                shape = self.vars["%s/%s" % (self.label, k)]["Shape"]
                 ishape = [int(x.strip(","), 16) for x in shape.strip().split()]
                 start = [0,] * len(ishape)
                 count = ishape
@@ -310,9 +244,6 @@ if __name__ == "__main__":
     world_size, world_rank = hydragnn.utils.setup_ddp()
     ##################################################################################################################
 
-    ## With MPI backend, the following should be called after ddp
-    if not MPI.Is_initialized():
-        MPI.Init()
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     comm_size = comm.Get_size()
@@ -358,25 +289,19 @@ if __name__ == "__main__":
         _valset = dataset_lists[1]
         _testset = dataset_lists[2]
 
-        adwriter = AdioGGO("ogb_gap_trainset.bp", comm)
-        adwriter.add(_trainset)
-        adwriter.save()
-
-        adwriter = AdioGGO("ogb_gap_valset.bp", comm)
-        adwriter.add(_valset)
-        adwriter.save()
-
-        adwriter = AdioGGO("ogb_gap_testset.bp", comm)
-        adwriter.add(_testset)
+        adwriter = AdioGGO("examples/ogb/dataset/ogb_gap.bp", comm)
+        adwriter.add("trainset", _trainset)
+        adwriter.add("valset", _valset)
+        adwriter.add("testset", _testset)
         adwriter.save()
 
         sys.exit(0)
 
     timer = Timer("load_data")
     timer.start()
-    trainset = OGBDataset("ogb_gap_trainset.bp")
-    valset = OGBDataset("ogb_gap_valset.bp")
-    testset = OGBDataset("ogb_gap_testset.bp")
+    trainset = OGBDataset("examples/ogb/dataset/ogb_gap.bp", "trainset")
+    valset = OGBDataset("examples/ogb/dataset/ogb_gap.bp", "valset")
+    testset = OGBDataset("examples/ogb/dataset/ogb_gap.bp", "testset")
 
     info("Adios load")
     info(
@@ -430,9 +355,6 @@ if __name__ == "__main__":
 
     hydragnn.utils.save_model(model, log_name)
     hydragnn.utils.print_timers(verbosity)
-
-    if not MPI.Is_finalized():
-        MPI.Finalize()
 
     sys.exit(0)
 
