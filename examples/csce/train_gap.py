30972ea0a7227a0921df23ff56bec3bc7142bd3e
Choi <choij@ornl.gov>
2022-05-27 16:51:51 -0400

    fix on log dir

--- examples/csce/train_gap.py
+++ examples/csce/train_gap.py
@@ -285,7 +285,7 @@ if __name__ == "__main__":
         optimizer, mode="min", factor=0.5, patience=5, min_lr=0.00001
     )
 
-    log_name = "ogb_" + inputfilesubstr + "_eV_fullx"
+    log_name = "csce_" + inputfilesubstr + "_eV_fullx"
     hydragnn.utils.setup_log(log_name)
     writer = hydragnn.utils.get_summary_writer(log_name)
     with open("./logs/" + log_name + "/config.json", "w") as f:
@@ -303,7 +303,7 @@ if __name__ == "__main__":
         config["NeuralNetwork"],
         log_name,
         verbosity,
-        create_plots=False,  ## Set false. CSCE data is too big
+        create_plots=True,
     )
 
     hydragnn.utils.save_model(model, optimizer, log_name)
