a07e03b793fad8b5eb8b07d97c709d28e90a510b
Choi <jyc@mac133386.tds.net>
2023-02-18 02:31:37 -0500

    wip: ci test debug

--- tests/test_model_loadpred.py
+++ tests/test_model_loadpred.py
@@ -13,6 +13,7 @@ import torch
 import random
 import hydragnn
 from .test_graphs import unittest_train_model
+import pytest
 
 
 def unittest_model_prediction(config):
@@ -58,6 +59,8 @@ def unittest_model_prediction(config):
 
 
 # test loading and predictiong of a saved model from previous training
+# it is possible that "config.json" can be overwritten. It can cause some error
+@pytest.mark.mpi_skip()
 def pytest_model_loadpred():
     model_type = "PNA"
     ci_input = "ci_multihead.json"
