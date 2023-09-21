3b0b38e324793d4b586d4bcee133f5c4ae7287f5
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2023-09-21 13:33:06 -0400

    restructure pre-process data utils (#192)
    * remove update_atom_features from method and use implementation of the same function from utils
    
    * function stratified_sampling moved to preprocess\utils
    
    * formatting fixed

--- hydragnn/preprocess/utils.py
+++ hydragnn/preprocess/utils.py
@@ -290,3 +290,47 @@ def update_atom_features(atom_features: [AtomFeatures], data: Data):
     """
     feature_indices = [i for i in atom_features]
     data.x = data.x[:, feature_indices]
+
+
+def stratified_sampling(dataset: [Data], subsample_percentage: float, verbosity=0):
+    """Given the dataset and the percentage of data you want to extract from it, method will
+    apply stratified sampling where X is the dataset and Y is are the category values for each datapoint.
+    In the case of the structures dataset where each structure contains 2 types of atoms, the category will
+    be constructed in a way: number of atoms of type 1 + number of protons of type 2 * 100.
+
+    Parameters
+    ----------
+    dataset: [Data]
+        A list of Data objects representing a structure that has atoms.
+    subsample_percentage: float
+        Percentage of the dataset.
+
+    Returns
+    ----------
+    [Data]
+        Subsample of the original dataset constructed using stratified sampling.
+    """
+    dataset_categories = []
+    print_distributed(verbosity, "Computing the categories for the whole dataset.")
+    for data in iterate_tqdm(dataset, verbosity):
+        frequencies = torch.bincount(data.x[:, 0].int())
+        frequencies = sorted(frequencies[frequencies > 0].tolist())
+        category = 0
+        for index, frequency in enumerate(frequencies):
+            category += frequency * (100 ** index)
+        dataset_categories.append(category)
+
+    subsample_indices = []
+    subsample = []
+
+    sss = StratifiedShuffleSplit(
+        n_splits=1, train_size=subsample_percentage, random_state=0
+    )
+
+    for subsample_index, rest_of_data_index in sss.split(dataset, dataset_categories):
+        subsample_indices = subsample_index.tolist()
+
+    for index in subsample_indices:
+        subsample.append(dataset[index])
+
+    return subsample
