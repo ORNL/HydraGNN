06dbb8c51e84f6bef85491e680f5c2117a6d98b0
Massimiliano Lupo Pasini <mlupopa@andes-login2.olcf.ornl.gov>
2024-03-19 23:53:33 -0400

    python script and training for mptrj set up

---
+++ examples/mptrj/download_data_andes.sh
@@ -0,0 +1,7 @@
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=http://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov'
+wget https://figshare.com/ndownloader/files/41619375
+mv 41619375 MPtrj_2022.9_full.json
