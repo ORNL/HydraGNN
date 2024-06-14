dcf4b12c44b2471fdbd3ee27539eda7e6d945eef
Massimiliano Lupo Pasini <mlupopa@login03.frontier.olcf.ornl.gov>
2024-06-14 08:16:01 -0400

    fix suggested by Jong

--- hydragnn/utils/adiosdataset.py
+++ hydragnn/utils/adiosdataset.py
@@ -107,6 +107,7 @@ class AdiosWriter:
                         break
 
                 for k in keys:
+                    vdim = self.comm.allreduce(0, op=MPI.MAX)
                     shape_list = self.comm.allgather([])
 
                 continue
