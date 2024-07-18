e1c3b85e4e941453168954383b5cb26593adfbf3
Zhifan Ye <yezhifan@mail.ustc.edu.cn>
2024-07-18 00:35:41 -0400

    mark mpi for deepspeed

--- tests/test_deepspeed.py
+++ tests/test_deepspeed.py
@@ -9,6 +9,7 @@ from tests.test_graphs import unittest_train_model
 
 # Test vector output
 @pytest.mark.parametrize("model_type", ["PNA"])
+@pytest.mark.mpi
 def pytest_train_model_vectoroutput_w_deepspeed(model_type, overwrite_data=False):
     unittest_train_model(
         model_type, "ci_vectoroutput.json", True, overwrite_data, use_deepspeed=True
@@ -16,8 +17,10 @@ def pytest_train_model_vectoroutput_w_deepspeed(model_type, overwrite_data=False
 
 
 # Test deepspeed zero
+# "3" is not supported by the gloo backend on the CI machine
 @pytest.mark.parametrize("model_type", ["PNA"])
 @pytest.mark.parametrize("zero_stage", ["1", "2"])
+@pytest.mark.mpi
 def pytest_train_model_vectoroutput_w_deepspeed_zero(
     model_type, zero_stage, overwrite_data=False
 ):
