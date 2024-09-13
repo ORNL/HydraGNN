57bc5cef3f9f88bf199004b113efdc9ebda4d255
Rylie Weaver <rylieweaver9@gmail.com>
2024-09-13 12:47:51 -0400

    formatting

--- examples/qm9/qm9.py
+++ examples/qm9/qm9.py
@@ -65,8 +65,8 @@ train, val, test = hydragnn.preprocess.split_dataset(
     dataset, config["NeuralNetwork"]["Training"]["perc_train"], False
 )
 (train_loader, val_loader, test_loader,) = hydragnn.preprocess.create_dataloaders(
-     train, val, test, config["NeuralNetwork"]["Training"]["batch_size"]
- )
+    train, val, test, config["NeuralNetwork"]["Training"]["batch_size"]
+)
 
 config = hydragnn.utils.update_config(config, train_loader, val_loader, test_loader)
 
