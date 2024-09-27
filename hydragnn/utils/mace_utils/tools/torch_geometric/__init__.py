7f8129de83cc3ad19102f89b5ea9cda1809f2ade
Rylie Weaver <rylieweaver9@gmail.com>
2024-09-27 12:13:34 -0400

    delete comments

--- hydragnn/utils/mace_utils/tools/torch_geometric/__init__.py
+++
@@ -1,8 +0,0 @@
-# from .batch import Batch
-# from .data import Data
-# from .dataloader import DataLoader
-# from .dataset import Dataset
-# from .seed import seed_everything
-
-# __all__ = ["Batch", "Data", "Dataset", "DataLoader", "seed_everything"]
-# __all__ = ["Data", "Dataset", "DataLoader", "seed_everything"]
