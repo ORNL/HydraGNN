ba5a8cec297bb1a1e3f9bf8d545ee6d423411e83
Rylie Weaver <rylieweaver9@gmail.com>
2024-11-27 09:06:07 -0500

    comment for allowed stacks in LJ force_grad

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -44,6 +44,9 @@ def pytest_examples_energy(example, model_type):
     assert return_code == 0
 
 
+# NOTE the grad forces example with LennardJones requires 
+#      there to be a positional gradient via using 
+#      positions in torch operations for message-passing.
 @pytest.mark.parametrize(
     "model_type",
     [
