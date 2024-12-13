e0e7fb664be7dd9cbcf289695d12fadd4e036f4f
allaffa <massimiliano.lupo.pasini@gmail.com>
2024-12-12 20:40:49 -0500

    SchNet threahsold increased for conv_head

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -164,6 +164,7 @@ def unittest_train_model(
         thresholds["PNAPlus"] = [0.2, 0.15]
     if ci_input == "ci_conv_head.json":
         thresholds["GIN"] = [0.25, 0.40]
+        thresholds["SchNet"] = [0.30, 0.30]
 
     verbosity = 2
 
