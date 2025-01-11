8196d99244fbdace18d7f406ab8bd7c07b732904
allaffa <massimiliano.lupo.pasini@gmail.com>
2025-01-10 21:59:05 -0500

    data attributes updated for consistency across datasets

--- examples/omat24/train.py
+++ examples/omat24/train.py
@@ -54,7 +54,6 @@ from fairchem.core.datasets import AseDBDataset
 def info(*args, logtype="info", sep=" "):
     getattr(logging, logtype)(sep.join(map(str, args)))
 
-
 # transform_coordinates = Spherical(norm=False, cat=False)
 transform_coordinates = LocalCartesian(norm=False, cat=False)
 # transform_coordinates = Distance(norm=False, cat=False)
