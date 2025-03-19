eec6e0f4beac55ca79b3dc2cdf1fa6632039723b
Jong Choi <jychoi-hpc@users.noreply.github.com>
2025-03-19 12:05:32 -0400

    Update energy_linear_regression.py
    Adding dataset_name

--- examples/multibranch/energy_linear_regression.py
+++ examples/multibranch/energy_linear_regression.py
@@ -173,6 +173,7 @@ if __name__ == "__main__":
     adwriter.add("testset", testset)
     adwriter.add_global("pna_deg", pna_deg)
     adwriter.add_global("energy_linear_regression_coeff", x)
+    adwriter.add_global("dataset_name", args.modelname.lower())
     adwriter.save()
 
     print("Done.")
