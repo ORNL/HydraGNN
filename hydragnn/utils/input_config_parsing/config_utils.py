edb16ad218704d1026f1854a6e1f7bdacd63796c
Rylie Weaver <rylieweaver9@gmail.com>
2025-05-19 01:42:20 +0000

    remove unnecessary job script and duplicate lines that default compute_grad_energy

--- hydragnn/utils/input_config_parsing/config_utils.py
+++ hydragnn/utils/input_config_parsing/config_utils.py
@@ -45,9 +45,6 @@ def update_config(config, train_loader, val_loader, test_loader):
         config["NeuralNetwork"]["Training"]["compute_grad_energy"] = False
 
     # This check is needed for update_config_NN_outputs
-    if "compute_grad_energy" not in config["NeuralNetwork"]["Training"]:
-        config["NeuralNetwork"]["Training"]["compute_grad_energy"] = False
-
     config["NeuralNetwork"] = update_config_NN_outputs(
         config["NeuralNetwork"], train_loader.dataset[0], graph_size_variable
     )
