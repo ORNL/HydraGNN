ecc48bdac2542d40cbbe9b17845149db6dcc4d4d
Arindam Chowdhury <52735221+ArCho48@users.noreply.github.com>
2025-07-21 12:19:06 -0400

    Update download_dataset.sh

--- examples/transition1x/download_dataset.sh
+++ examples/transition1x/download_dataset.sh
@@ -1,1 +1,1 @@
-wget https://figshare.com/ndownloader/files/36035789 -O transition1x-release.h5
+wget --user-agent="Mozilla" https://figshare.com/ndownloader/files/36035789 -O transition1x-release.h5
