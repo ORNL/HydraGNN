5b606864f480d0a432d59db8c53eeab497a9b518
Jong Choi <choij@ornl.gov>
2025-07-26 10:52:14 -0400

    add XPU

--- hydragnn/utils/profiling_and_tracing/tracer.py
+++ hydragnn/utils/profiling_and_tracing/tracer.py
@@ -13,6 +13,7 @@ from mpi4py import MPI
 from collections import defaultdict
 import numpy as np
 from hydragnn.utils.distributed import get_comm_size_and_rank
+import os
 
 
 class Tracer(ABC):
@@ -42,12 +43,21 @@ try:
     class GPTLTracer(Tracer):
         def __init__(self, **kwargs):
             gp.initialize()
+            self.hist = dict()
+            self.last = dict()
 
         def start(self, name):
             gp.start(name)
 
         def stop(self, name):
             gp.stop(name)
+
+            count, wallclock = gp.query_raw(name)
+            if name not in self.hist:
+                self.hist[name] = list()
+                self.last[name] = 0.0
+            self.hist[name].append((count, wallclock - self.last[name]))
+            self.last[name] = wallclock
 
         def enable(self):
             gp.enable()
@@ -57,6 +67,8 @@ try:
 
         def reset(self):
             gp.reset()
+            self.hist = dict()
+            self.last = dict()
 
 
 except:
@@ -97,9 +109,7 @@ try:
             nvmlInit()
 
             deviceCount = nvmlDeviceGetCount()
-            if os.getenv("ROCR_VISIBLE_DEVICES"):
-                self.device = int(os.getenv("ROCR_VISIBLE_DEVICES").split(",")[0])
-            elif os.getenv("CUDA_VISIBLE_DEVICES"):
+            if os.getenv("CUDA_VISIBLE_DEVICES"):
                 self.device = int(os.getenv("CUDA_VISIBLE_DEVICES").split(",")[0])
             else:
                 self.device = 0
@@ -109,18 +119,19 @@ try:
             self.enabled = True
             print(f"NVMLTracer initalized: rank={self.rank}, device={self.device}")
 
+        ## nvmlDeviceGetTotalEnergyConsumption returns in mJ. Use uJ
         def start(self, name):
             if not self.enabled:
                 return
-            self.energyCounters[name] = nvmlDeviceGetTotalEnergyConsumption(
-                self.d_handle
+            self.energyCounters[name] = (
+                nvmlDeviceGetTotalEnergyConsumption(self.d_handle) * 1_000
             )
 
         def stop(self, name):
             if not self.enabled:
                 return
             self.energyCounters[name] = (
-                nvmlDeviceGetTotalEnergyConsumption(self.d_handle)
+                nvmlDeviceGetTotalEnergyConsumption(self.d_handle) * 1_000
                 - self.energyCounters[name]
             )
             self.energyTracer[name].append(self.energyCounters[name])
@@ -159,7 +170,6 @@ except:
 
 try:
     ## Ref: https://rocm.docs.amd.com/projects/rocm_smi_lib/en/develop/tutorials/python_tutorials.html
-    import os
     import sys
 
     ROCM_PATH = os.getenv("ROCM_PATH", "/opt/rocm")
@@ -175,19 +185,6 @@ try:
         except ValueError:
             return np.nan
 
-    def get_energy(rocmsmi, device):
-        """Accumulated Energy (uJ)"""
-        try:
-            power = c_uint64()
-            timestamp = c_uint64()
-            counter_resolution = c_float()
-            ret = rocmsmi.rsmi_dev_energy_count_get(
-                device, byref(power), byref(counter_resolution), byref(timestamp)
-            )
-            return power.value * counter_resolution.value
-        except:
-            return np.nan
-
     class ROCMTracer:
         def __init__(self, **kwargs):
             rocm_smi.initializeRsmi()
@@ -199,22 +196,102 @@ try:
 
             if os.getenv("ROCR_VISIBLE_DEVICES"):
                 self.device = int(os.getenv("ROCR_VISIBLE_DEVICES").split(",")[0])
-            elif os.getenv("CUDA_VISIBLE_DEVICES"):
-                self.device = int(os.getenv("CUDA_VISIBLE_DEVICES").split(",")[0])
             else:
                 self.device = rocm_smi.listDevices()[0]
             print(f"ROCMTracer initalized: rank={self.rank}, device={self.device}")
 
+        def get_energy(self):
+            """Accumulated Energy (uJ)"""
+            try:
+                power = c_uint64()
+                timestamp = c_uint64()
+                counter_resolution = c_float()
+                ret = self.rocmsmi.rsmi_dev_energy_count_get(
+                    self.device,
+                    byref(power),
+                    byref(counter_resolution),
+                    byref(timestamp),
+                )
+                return power.value * counter_resolution.value
+            except:
+                return np.nan
+
         def start(self, name):
             if not self.enabled:
                 return
-            self.energyCounters[name] = get_energy(self.rocmsmi, self.device)
+            self.energyCounters[name] = self.get_energy()
+
+        def stop(self, name):
+            if not self.enabled:
+                return
+            self.energyCounters[name] = self.get_energy() - self.energyCounters[name]
+            self.energyTracer[name].append(self.energyCounters[name])
+
+        def enable(self):
+            self.enabled = True
+
+        def disable(self):
+            self.enabled = False
+
+        def reset(self):
+            self.energyCounters = dict()
+            self.energyTracer = defaultdict(list)
+
+        def pr_file(self, file_path):
+            dirname = os.path.dirname(file_path)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname, exist_ok=True)
+            with open(file_path, mode="w", encoding="utf-8") as file:
+                file.write("name, ncalls, mean, total, median, std_dev, max, min\n")
+                for k, v in self.energyTracer.items():
+                    mean_energy = np.mean(v)
+                    total_energy = np.sum(v)
+                    median_energy = np.median(v)
+                    stdDev = np.std(v)
+                    max_energy = np.max(v)
+                    min_energy = np.min(v)
+
+                    file.write(
+                        f"{k}, {len(v)}, {mean_energy}, {total_energy}, {median_energy}, {stdDev}, {max_energy}, {min_energy}\n"
+                    )
+
+
+except:
+    pass
+
+try:
+
+    class XPUTracer:
+        def __init__(self, **kwargs):
+            self.rank = MPI.COMM_WORLD.Get_rank()
+            self.device = torch.xpu.current_device()
+            group = self.device // 2
+            counter_id = group * 3 + self.device % 2 + 1
+            counter = f"/sys/class/hwmon/hwmon{counter_id}/energy1_input"
+            self.f = open(counter, "r")
+
+            self.energyCounters = dict()
+            self.energyTracer = defaultdict(list)
+            self.enabled = True
+
+            print(f"XPUTracer initalized: rank={self.rank}, device={self.device}")
+
+        def get_energy_read(self):
+            ## Cumulative energy used (uJ)
+            self.f.seek(0)
+            energy_uj = float(self.f.read().strip())
+            return energy_uj
+
+        def start(self, name):
+            if not self.enabled:
+                return
+            self.energyCounters[name] = self.get_energy_read()
 
         def stop(self, name):
             if not self.enabled:
                 return
             self.energyCounters[name] = (
-                get_energy(self.rocmsmi, self.device) - self.energyCounters[name]
+                self.get_energy_read() - self.energyCounters[name]
             )
             self.energyTracer[name].append(self.energyCounters[name])
 
@@ -250,6 +327,7 @@ try:
 except:
     pass
 
+
 __tracer_list__ = dict()
 
 
@@ -258,7 +336,7 @@ def has(name):
 
 
 def initialize(
-    trlist=["GPTLTracer", "SCOREPTracer", "NVMLTracer", "ROCMTracer"],
+    trlist=["GPTLTracer", "SCOREPTracer", "NVMLTracer", "ROCMTracer", "XPUTracer"],
     verbose=False,
     **kwargs,
 ):
@@ -316,8 +394,16 @@ def save(log_name):
     if has("GPTLTracer"):
         import gptl4py as gp
 
+        tx = __tracer_list__["GPTLTracer"]
+
         gp.pr_file(os.path.join("logs", log_name, "gp_timing.p%d" % rank))
         gp.pr_summary_file(os.path.join("logs", log_name, "gp_timing.summary"))
+
+        with open(os.path.join("logs", log_name, f"gp_full.p{rank}"), "w") as f:
+            f.write("rank,label,count,wallclock\n")
+            for key, value_list in tx.hist.items():
+                for count, wallclock in value_list:
+                    f.write(f"{rank},{key},{count},{wallclock}\n")
 
     if has("NVMLTracer"):
         tx = __tracer_list__["NVMLTracer"]
@@ -326,6 +412,10 @@ def save(log_name):
     if has("ROCMTracer"):
         tx = __tracer_list__["ROCMTracer"]
         tx.pr_file(os.path.join("logs", log_name, "rocm_energy.p%d" % rank))
+
+    if has("XPUTracer"):
+        tx = __tracer_list__["XPUTracer"]
+        tx.pr_file(os.path.join("logs", log_name, "xpu_energy.p%d" % rank))
 
 
 def profile(x_or_func=None, *decorator_args, **decorator_kws):
