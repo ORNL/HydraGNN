798f84df15d8927ae4f80d11e296e3b166e61d01
allaffa <massimiliano.lupo.pasini@gmail.com>
2025-09-12 12:00:16 -0400

    removed import of InteratomicPotential from hydragnn/models/__init__.py

--- hydragnn/models/__init__.py
+++ hydragnn/models/__init__.py
@@ -5,4 +5,3 @@ from .PNAStack import PNAStack
 from .GINStack import GINStack
 from .create import create_model, create_model_config
 from .MultiTaskModelMP import MultiTaskModelMP
-from .InteratomicPotential import InteratomicPotentialMixin, InteratomicPotentialBase
