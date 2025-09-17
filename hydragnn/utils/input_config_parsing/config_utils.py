cc0c8b798ef48875e9b7a841d28a95bb348e4318
Rylie Weaver <rylieweaver9@gmail.com>
2025-09-17 15:41:24 -0400

    black

--- hydragnn/utils/input_config_parsing/config_utils.py
+++ hydragnn/utils/input_config_parsing/config_utils.py
@@ -166,8 +166,10 @@ def update_config_equivariance(config):
     equivariance_toggled_models = ["EGNN"]
     if "equivariance" in config:
         if config["mpnn_type"] not in equivariance_toggled_models:
-            warnings.warn(f"E(3) equivariance can only be toggled for EGNN. Setting it for {config['mpnn_type']} won't break anything,"
-                          "but won't change anything either.")
+            warnings.warn(
+                f"E(3) equivariance can only be toggled for EGNN. Setting it for {config['mpnn_type']} won't break anything,"
+                "but won't change anything either."
+            )
     else:
         config["equivariance"] = None
     return config
