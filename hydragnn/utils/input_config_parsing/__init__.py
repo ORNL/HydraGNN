d9e781f65d2c45cb9d29c349a2f17c23106a33c6
OpenEquivariance Integration <openequivariance@hydragnn.org>
2025-10-08 22:14:19 -0400

    update_config_equivariance included

--- hydragnn/utils/input_config_parsing/__init__.py
+++ hydragnn/utils/input_config_parsing/__init__.py
@@ -2,7 +2,7 @@ from .config_utils import (
     update_config,
     update_config_minmax,
     update_config_edge_dim,
-    update_config_node_dim,
+    update_config_equivariance,
     get_log_name_config,
     save_config,
     parse_deepspeed_config,
