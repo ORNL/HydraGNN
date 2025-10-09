56893094e94e8159116ff711cbc6e590a6df8eac
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2025-10-09 09:54:05 -0400

    update_config_functions included in init for fine-tuning (#376)
    * update_config_functions included in init for fine-tuning
    
    * update_config_equivariance included
    
    ---------
    
    Co-authored-by: OpenEquivariance Integration <openequivariance@hydragnn.org>

--- hydragnn/utils/input_config_parsing/__init__.py
+++ hydragnn/utils/input_config_parsing/__init__.py
@@ -1,6 +1,8 @@
 from .config_utils import (
     update_config,
     update_config_minmax,
+    update_config_edge_dim,
+    update_config_equivariance,
     get_log_name_config,
     save_config,
     parse_deepspeed_config,
