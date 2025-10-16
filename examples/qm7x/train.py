2610b206d154e5d0b44aae94e49aa70c377fbfdd
OpenEquivariance Integration <openequivariance@hydragnn.org>
2025-10-16 12:01:10 -0400

    formatting fixed

--- examples/qm7x/train.py
+++ examples/qm7x/train.py
@@ -614,6 +614,6 @@ if __name__ == "__main__":
             gp.pr_file(os.path.join("logs", log_name, "gp_timing.p%d" % rank))
         gp.pr_summary_file(os.path.join("logs", log_name, "gp_timing.summary"))
         gp.finalize()
-    
+
     dist.destroy_process_group()
     sys.exit(0)
