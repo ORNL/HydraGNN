6b6f23e51e685d4c809c461e5b2029ef5fa3ed5b
Massimiliano Lupo Pasini <mlupopa@andes-login3.olcf.ornl.gov>
2026-01-05 10:18:21 -0500

    updated download dataset script for alexandria

--- examples/alexandria/download_dataset.sh
+++ examples/alexandria/download_dataset.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# URL to download from
-URL="https://alexandria.icams.rub.de/data/"
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=http://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov'
+
+# List of URLs to download from
+URLS=(
+  "https://alexandria.icams.rub.de/data/pbe/2024.12.15/"
+  "https://alexandria.icams.rub.de/data/pbe_2d/"
+  "https://alexandria.icams.rub.de/data/pbe_1d/"
+)
 
 # Directory where files will be saved
 OUTPUT_DIR="./dataset/compressed_data"
@@ -9,17 +19,22 @@ OUTPUT_DIR="./dataset/compressed_data"
 # Create output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
-# Use wget to recursively download all files and directories
-wget --recursive \
-     --no-parent \
-     --continue \
-     --no-clobber \
-     --convert-links \
-     --cut-dirs=1 \
-     --no-check-certificate \
-     --reject-regex="(/older/|/geo_opt_paths/)" \
-     --reject "*index.html*" \
-     --directory-prefix="$OUTPUT_DIR" \
-     "$URL"
+# Loop over all URLs
+for URL in "${URLS[@]}"; do
+  echo "Downloading from $URL"
+
+  wget --recursive \
+       --no-parent \
+       --continue \
+       --no-clobber \
+       --convert-links \
+       --cut-dirs=1 \
+       --no-check-certificate \
+       --reject-regex="(/older/|/geo_opt_paths/)" \
+       --reject "*index.html*" \
+       --directory-prefix="$OUTPUT_DIR" \
+       "$URL"
+done
 
 echo "Download complete. All files saved to $OUTPUT_DIR."
+
