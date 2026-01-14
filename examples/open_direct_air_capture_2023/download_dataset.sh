3eebd6c2f1c2d53ae3c28d6182c3b16d9bacab1e
Massimiliano Lupo Pasini <mlupopa@andes-login1.olcf.ornl.gov>
2026-01-13 21:49:16 -0500

    alexandria download datasets and open catalyst 2020 train scripts udpated

--- examples/open_direct_air_capture_2023/download_dataset.sh
+++ examples/open_direct_air_capture_2023/download_dataset.sh
@@ -1,3 +1,8 @@
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=http://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov'
 
 wget -c https://dl.fbaipublicfiles.com/large_objects/dac/datasets/extxyz_train.tar.gz
 wget -c https://dl.fbaipublicfiles.com/dac/datasets/extxyz_val.tar.gz
