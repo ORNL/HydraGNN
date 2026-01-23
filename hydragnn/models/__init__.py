d600b96930c1441a837d7fbed1b6a00721183092
Jong Choi <jychoi-hpc@users.noreply.github.com>
2026-01-23 14:53:08 -0500

    using fsdp optimizer (#445)

--- hydragnn/models/__init__.py
+++ hydragnn/models/__init__.py
@@ -4,4 +4,4 @@ from .GINStack import GINStack
 from .PNAStack import PNAStack
 from .GINStack import GINStack
 from .create import create_model, create_model_config
-from .MultiTaskModelMP import MultiTaskModelMP
+from .MultiTaskModelMP import MultiTaskModelMP, DualOptimizer
