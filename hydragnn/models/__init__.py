4d1617f78f8960b0aca02e048735120ae75bb7c0
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2026-01-28 16:09:27 -0500

    heterogeneous GNN and OPF example added

--- hydragnn/models/__init__.py
+++ hydragnn/models/__init__.py
@@ -3,5 +3,12 @@ from .GATStack import GATStack
 from .GINStack import GINStack
 from .PNAStack import PNAStack
 from .GINStack import GINStack
+from .heterogeneous import (
+    HeteroBase,
+    HeteroGINStack,
+    HeteroSAGEStack,
+    HeteroGATStack,
+    HeteroPNAStack,
+)
 from .create import create_model, create_model_config
 from .MultiTaskModelMP import MultiTaskModelMP, DualOptimizer
---
+++ hydragnn/models/heterogeneous/__init__.py
@@ -0,0 +1,5 @@
+from .HeteroBase import HeteroBase
+from .HeteroGINStack import HeteroGINStack
+from .HeteroSAGEStack import HeteroSAGEStack
+from .HeteroGATStack import HeteroGATStack
+from .HeteroPNAStack import HeteroPNAStack
