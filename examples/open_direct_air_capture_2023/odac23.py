a1248f365e37c9b00e5a1bd5180ddcc6aee6c1fc
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2026-02-02 17:09:06 -0500

    Local Cartesian replaced witrh Distance on ODAC23 dataset (#453)

--- examples/open_direct_air_capture_2023/odac23.py
+++ examples/open_direct_air_capture_2023/odac23.py
@@ -19,11 +19,11 @@ from hydragnn.utils.distributed import nsplit
 
 
 # transform_coordinates = Spherical(norm=False, cat=False)
-transform_coordinates = LocalCartesian(norm=False, cat=False)
-# transform_coordinates = Distance(norm=False, cat=False)
+# transform_coordinates = LocalCartesian(norm=False, cat=False)
+transform_coordinates = Distance(norm=False, cat=False)
 
-transform_coordinates_pbc = PBCLocalCartesian(norm=False, cat=False)
-# transform_coordinates_pbc = PBCDistance(norm=False, cat=False)
+# transform_coordinates_pbc = PBCLocalCartesian(norm=False, cat=False)
+transform_coordinates_pbc = PBCDistance(norm=False, cat=False)
 
 # charge and spin are constant across MPTrj dataset
 charge = 0.0  # neutral
