6893fa493fdc9639dc7f7637bc003aa3e6b7ed54
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2026-02-12 15:12:28 -0500

    updated all HPO for specifci MPNN type to capture all datasets

--- examples/multidataset_hpo_sc26/gfm_deephyper_multi_egnn.py
+++ examples/multidataset_hpo_sc26/gfm_deephyper_multi_egnn.py
@@ -80,7 +80,7 @@ def run(trial, dequed=None):
             "--inputfile=gfm_mlip.json",
             "--multi",
             "--ddstore",
-            '--multi_model_list="ANI1x,MPTrj,OC2020,OC2022,ODAC23,OMat24,OMol25,OC2025,OPoly2026,nabla2dft,QCML,qm7x,transition1x"',
+            '--multi_model_list="Alexandria,ANI1x,MPTrj,OC2020,OC2022,OC25,ODAC23,OMat24,OMol25,OMol25-neutral,OMol25-non-neutral,OC2025,OPoly2026,Nabla2DFT,QCML,QM7-X,transition1x"',
             "--num_epoch=10",
             f"--log={log_name}",
             f"--learning_rate={trial.parameters['learning_rate']}",
