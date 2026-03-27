e680705ed93c657a9414e4cc725bca6f00a877d4
Massimiliano Lupo Pasini <mlupopa@login11.frontier.olcf.ornl.gov>
2026-03-27 12:32:31 -0400

    Fix black formatting in datasets __init__

--- hydragnn/utils/datasets/__init__.py
+++ hydragnn/utils/datasets/__init__.py
@@ -21,6 +21,7 @@ from .compositional_data_splitting import (
     compositional_stratified_splitting,
 )
 from .distdataset import DistDataset
+
 try:
     from .hdf5dataset import HDF5Dataset, HDF5Writer
 except ImportError:
