4b5d4fad0be198d6af02490366c0126704824281
Massimiliano Lupo Pasini <mlupopa@login11.frontier.olcf.ornl.gov>
2026-03-27 12:29:11 -0400

    Make h5py import optional in datasets __init__ to fix CI without h5py

--- hydragnn/utils/datasets/__init__.py
+++ hydragnn/utils/datasets/__init__.py
@@ -13,7 +13,10 @@ from .compositional_data_splitting import (
     compositional_stratified_splitting,
 )
 from .distdataset import DistDataset
-from .hdf5dataset import HDF5Dataset, HDF5Writer
+try:
+    from .hdf5dataset import HDF5Dataset, HDF5Writer
+except ImportError:
+    pass
 from .lsmsdataset import LSMSDataset
 from .pickledataset import SimplePickleDataset, SimplePickleWriter
 from .serializeddataset import SerializedDataset, SerializedWriter
