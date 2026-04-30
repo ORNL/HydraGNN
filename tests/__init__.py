e0b059e74029090ab30315b46e458a883d7724d0
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2026-04-30 17:10:26 -0400

    Add HydraGNN BSD-3-clause copyright header to all .py files
    Adds the standard 10-line BSD-3-clause header to every Python file
    that lacked one. The year reflects the file's last git-commit year
    (falls back to mtime). Vendored FairChem trees under
    examples/*/fairchem/core/** are intentionally skipped; they retain
    Meta's MIT header.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Copyright (c) 2022, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
 from .deterministic_graph_data import deterministic_graph_data
 from .test_config import pytest_config
 from .test_graphs import unittest_train_model
