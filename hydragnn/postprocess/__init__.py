387f596710b303d2020d445e881cfe3530ba536a
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2026-04-30 17:10:26 -0400

    Add HydraGNN BSD-3-clause copyright header to all .py files
    Adds the standard 10-line BSD-3-clause header to every Python file
    that lacked one. The year reflects the file's last git-commit year
    (falls back to mtime). Vendored FairChem trees under
    examples/*/fairchem/core/** are intentionally skipped; they retain
    Meta's MIT header.

--- hydragnn/postprocess/__init__.py
+++ hydragnn/postprocess/__init__.py
@@ -1,2 +1,12 @@
+##############################################################################
+# Copyright (c) 2021, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
 from .visualizer import Visualizer
 from .postprocess import output_denormalize, unscale_features_by_num_nodes
--- hydragnn/utils/optimizer/optimizer.py
+++ hydragnn/utils/optimizer/optimizer.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Copyright (c) 2025, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
 import torch
 from hydragnn.utils.distributed import get_device_name
 from torch.distributed.optim import ZeroRedundancyOptimizer
