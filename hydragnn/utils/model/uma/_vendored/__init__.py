74352e722118e56193ea14a86831a1948f10e56a
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2026-08-19 15:54:56 -0400

    Add 2026 copyright headers to new Python files

--- hydragnn/utils/model/uma/_vendored/__init__.py
+++ hydragnn/utils/model/uma/_vendored/__init__.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Copyright (c) 2026, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
 # NOTE: This tree was vendored verbatim from fairchem-core (Meta MIT-licensed)
 # for use inside HydraGNN. Imports were rewritten by
 # tools/vendor_uma.py to point at hydragnn.utils.model.uma._vendored.
--- tests/test_fairchem_native_parity.py
+++ tests/test_fairchem_native_parity.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Copyright (c) 2026, Oak Ridge National Laboratory                          #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of HydraGNN and is distributed under a BSD 3-clause      #
+# license. For the licensing terms see the LICENSE file in the top-level     #
+# directory.                                                                 #
+#                                                                            #
+# SPDX-License-Identifier: BSD-3-Clause                                      #
+##############################################################################
 """Optional numerical parity tests against a native FAIR-Chem installation.
 
 These tests compare backbone and normalization boundaries with identical
